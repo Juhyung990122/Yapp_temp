04291fdfbeaeb833c3a0f60a6fe972691aa24b9c
Juhyung990122 <harukawa99@naver.com>
2020-10-22 22:33:37 +0900

    Add report_user

--- User/views.py
+++ User/views.py
@@ -25,7 +25,7 @@ class MgmtUserViewSet(viewsets.ModelViewSet):
         self.perform_update(user_info)
         return Response(status=status.HTTP_201_CREATED)
 
-    #3일 미접속 유저 user state 변경 
+    #3일 미접속 유저 user state 변경 , 신고유저 state 변경 (cron 으로 자동화 예정)
     @action(detail=True, methods=['get'])
     def change_userstate(self,request, pk, *args, **kwargs):
         user_info = self.get_object()
@@ -33,6 +33,19 @@ class MgmtUserViewSet(viewsets.ModelViewSet):
             user_info.state = "D"
             serializer = self.get_serializer(user_info)
             return Response(serializer.data)
+
+    #신고기능_유저
+    @action(detail=True, methods=['get'])
+    def report_user(self,request, pk, *args, **kwargs):
+        user_info = self.get_object()
+        user_info.report_user_cnt += 1
+        self.perform_update(user_info)
+
+        if user_info.report_user_cnt >= 3:
+            #self.perform_destroy(user_info)
+            user_info.state = "L"
+            serializer = self.get_serializer(user_info)
+            return Response(serializer.data)    
 
 class FeedViewSet(viewsets.ModelViewSet):
     queryset = Feed.objects.all()
